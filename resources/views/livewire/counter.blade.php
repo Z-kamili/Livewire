255db57f04f048b32a7254d1b7be0e028bf44ce9
Z-Kamili <zakariakamili97@gmail.com>
2022-02-16 21:20:11 +0100

    first commit

---
+++ resources/views/livewire/counter.blade.php
@@ -0,0 +1,12 @@
+<div class="flex">
+    <div class="m-auto">
+        <span>{{$count}}</span>
+   <button wire:click="decrement" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+    -
+  </button>
+  <button wire:click="increment" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+    +
+  </button>
+    </div>
+   
+</div>
